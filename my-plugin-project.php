048a15728f64f16772499c8ab1955d70122a1a86
Dexter Gal <109516607+dextergal@users.noreply.github.com>
2024-10-28 16:17:18 +0800

    Update my-plugin-project.php

--- my-plugin-project.php
+++ my-plugin-project.php
@@ -2,6 +2,7 @@
 /*
  * Plugin Name:       Remove Toolbar Frontend
  * Description:       This plugin removes the toolbar from the frontend.
+ * Version:           1.0.0
  * Author:            Dexter Gal
  */
 
