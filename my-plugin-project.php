61e8b9e86fd109764683552771c5e03327d79868
Dexter Gal <109516607+dextergal@users.noreply.github.com>
2024-10-28 16:08:01 +0800

    Update my-plugin-project.php

--- my-plugin-project.php
+++ my-plugin-project.php
@@ -1,5 +1,10 @@
 <?php
 /*
- * Plugin Name: YOUR PLUGIN NAME
+ * Plugin Name: Dex Plugin
+ * Plugin Name:       My Basics Plugin
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       Handle the basics with this plugin.
  */
-?>
+
+ /* Disable WordPress Admin Bar for all users */
+add_filter( 'show_admin_bar', '__return_false' );
